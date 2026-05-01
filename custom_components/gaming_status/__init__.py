27fe84493489bd4b6af06a8c41bf426bf849efc6
adamjthompson <ohidjwae@gmail.com>
2026-05-01 09:30:50 -0400

    Updated version number.

--- custom_components/gaming_status/__init__.py
+++ custom_components/gaming_status/__init__.py
@@ -89,7 +89,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         sidebar_title="Gaming Status",
         sidebar_icon="mdi:controller",
         frontend_url_path="gaming-status-config",
-        config={"url": "/gaming_status/configurator?v=174"}, 
+        config={"url": "/gaming_status/configurator?v=175"}, 
         require_admin=True,
     )
 
